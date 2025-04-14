aefe95d5b8caad6def20df1203ba38f636ce5f1a
Vanessa Dos Santos <smilzvanessa@gmail.com>
2025-04-14 14:57:25 +0200

    made better comments

--- Module 01/ex02/main.cpp
+++ Module 01/ex02/main.cpp
@@ -3,16 +3,16 @@
 int main()
 {
 	std::string s = "HI THIS IS BRAIN";
-	std::string *strPTR = &s; // a pointer to the string.
-	std::string &strREF = s; // reference to the string
+	std::string *strPTR = &s; // A pointer to s
+	std::string &strREF = s; // Reference to s
 
-	std::cout << &s << std::endl; // The memory address of the string variable
+	std::cout << &s << std::endl; // The memory address of the s
 	std::cout << strPTR << std::endl; // The memory address held by stringPTR
 	std::cout << &strREF << std::endl; // The memory address held by stringREF
 
-	std::cout << s << std::endl;
-	std::cout << *strPTR << std::endl;
-	std::cout << strREF << std::endl;
+	std::cout << s << std::endl; // Value of s
+	std::cout << *strPTR << std::endl; // Value of what stringPTR points to
+	std::cout << strREF << std::endl; // Value that strREF refers to
 
 	return 0;
 }
