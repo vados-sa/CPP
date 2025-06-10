77572e975bb001c13cd6a95b1961f39387fe0bcb
Vanessa Dos Santos <smilzvanessa@gmail.com>
2025-06-10 17:16:53 +0200

    ort. can. form

--- Module 06/ex00/ScalarConverter.cpp
+++ Module 06/ex00/ScalarConverter.cpp
@@ -1,23 +1,71 @@
 #include "ScalarConverter.hpp"
 
-/* static_cast<new_type>(exp); */
+ScalarConverter::ScalarConverter()
+{
+	std::cout << "Constructor" << std::endl;
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter& other)
+{
+	std::cout << "Copy constructor" << std::endl;
+}
+
+ScalarConverter& ScalarConverter::operator = (const ScalarConverter& other)
+{
+	std::cout << "Copy assignment operator" << std::endl;
+	if (this != &other)
+		return *this;
+}
+
+ScalarConverter::~ScalarConverter()
+{
+	std::cout << "Destructor" << std::endl;
+}
+
+/* IT'S WAY BIGGER THAN I THOUGHT
+
+
+- detect the type pf the literal passed;
+- convert to its actual type;
+- convert to the other three data types;
+- display the results (char, int, float, double)*/
 
 void ScalarConverter::convert(std::string str)
 {
-	int int_value;
-	std::stringstream(str) >> int_value;
-
-	char c =  static_cast<char>(int_value);
-	std::cout << "String as char: " << c << std::endl;
-	/* if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	// char
+	char c = 0;
+	if (str.length() == 1)
+		c = static_cast<char>(str[0]);
+	else if ((str[0] == '+' || str[0] == '-') && str.length() == 2 && std::isdigit(str[1]))
+		c = static_cast<char>(str[1]);
+	if ((c >= 7 && c <= 13) || (c >= 32 && c <= 126))
+	std::cout << "char: " << c << std::endl;
 	else
-		std::cout << "A conversion to char is not displayable." << std::endl; */
-
-	std::cout << "String as int: " << int_value << std::endl;
+	std::cout << "Non displayable" << std::endl;
 	
-	std::cout << "String as float: " << static_cast<float>(int_value) << std::endl;
+	// int
+	int int_value;
+	if (str.length() == 1 && !std::isdigit(str[0]))
+		int_value = static_cast<int>(str[0]); // gets ascii value
+	else
+		std::stringstream(str) >> int_value;
+	std::cout << "int: " << int_value << std::endl;
 	
-	std::cout << "String as double: " << static_cast<double>(int_value) << std::endl;
-
+	
+	// float
+	float float_value;
+	if (str.length() == 1 && !std::isdigit(str[0]))
+		float_value = static_cast<float>(str[0]); // gets ascii value
+	else
+		std::stringstream(str) >> float_value;
+	std::cout << "float: " << std::fixed << std::setprecision(1) << float_value << "f" << std::endl;
+	
+	// double
+	double double_value;
+	if (str.length() == 1 && !std::isdigit(str[0]))
+		double_value = static_cast<double>(str[0]); // gets ascii value
+	else
+		std::stringstream(str) >> double_value;
+	std::cout << "double: " << std::setprecision(1) << double_value << std::endl;
 }
 
--- Module 06/ex00/main.cpp
+++ Module 06/ex00/main.cpp
@@ -2,5 +2,23 @@
 
 int main()
 {
-	ScalarConverter::convert("42");
+	std::cout << "Converting a number:" << std::endl;
+	ScalarConverter::convert("42.0f");
+
+	std::cout << std::endl;
+
+	std::cout << "Converting a character:" << std::endl;
+	ScalarConverter::convert("A");
+
+	std::cout << std::endl;
+
+	std::cout << "Converting pseudo-literals f:" << std::endl;
+	ScalarConverter::convert("+inff");
+
+	std::cout << std::endl;
+
+	std::cout << "Converting pseudo-literals d:" << std::endl;
+	ScalarConverter::convert("+inf");
+	/* std::cout << "Converting a string:" << std::endl; // not sure if this should be handled
+	ScalarConverter::convert("hello"); */
 }
