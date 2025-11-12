7e16642f119c56b6d73b3a87eb20d8b8cde5be2f
Vanessa dos Santos <smilzvanessa@gmail.com>
2025-11-12 13:37:47 +0100

    Error message for wrong number of arguments

--- Module 09/ex02/main.cpp
+++ Module 09/ex02/main.cpp
@@ -5,10 +5,18 @@
 // Your program must use the merge-insert sort algorithm to sort the positive integer sequence.
 // If an error occurs during program execution, an error message should be displayed on the standard error.
 
+/*
+Build two containers with the same data (pick two distinct STL containers).
+Sort both using the same Ford–Johnson algorithm (not std::sort).
+Print “Before:” (original), “After:” (sorted).
+Measure and print time for each container (same machine, same input). */
+
 
 int main(int ac, char *av[]) {
-	if (ac == 1)
+	if (ac == 1) {
+		std::cerr << "Error: Wrong number of arguments" << std::endl;
 		return 1;
+	}
 
 	try
 	{
@@ -20,4 +28,5 @@ int main(int ac, char *av[]) {
 		std::cerr << "Error: " << e.what() << '\n';
 	}
 	
+	return 0;
 }
