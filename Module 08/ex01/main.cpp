97932b141b28294c46bfc7a0084101e7547eb951
Vanessa dos Santos <smilzvanessa@gmail.com>
2025-11-17 12:53:24 +0100

    complete tests

--- Module 08/ex01/main.cpp
+++ Module 08/ex01/main.cpp
@@ -1,10 +1,9 @@
 #include "Span.hpp"
 #include <iostream>
 
-//  Test your Span with at least 10,000 numbers. More would be even better.
+void school_test() {
+	std::cout << "---- 42 test ----" << std::endl;
 
-int main()
-{
 	Span sp = Span(5);
 	
 	sp.addNumber(6);
@@ -13,7 +12,69 @@ int main()
 	sp.addNumber(9);
 	sp.addNumber(11);
 
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
+	std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+	std::cout << "Longest span: " << sp.longestSpan() << std::endl;
+}
+
+void test_big_sequential() {
+	std::cout << "---- Test more than 10000 numbers ----" << std::endl;
+
+	Span sp = Span(10002);
+	
+	for (int i = 0; i < sp.getN(); i++)
+		sp.addNumber(i);
+		
+	std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+	std::cout << "Longest span: " << sp.longestSpan() << std::endl;
+	
+	std::cout << "Try to add a 10003rd number: ";
+	sp.addNumber(10003);
+}
+
+void test_big_addRange() {
+	std::cout << "---- Test adding range ----" << std::endl;
+
+	std::vector<int> range;
+
+	for (int i = 0; i < 1000; i++)
+		range.push_back(i);
+	
+	Span sp = Span(1000);
+	sp.addNumbersRange(range.begin(), range.end());
+
+	std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+	std::cout << "Longest span: " << sp.longestSpan() << std::endl;
+}
+
+int main()
+{
+	try
+	{
+		school_test();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	try
+	{
+		test_big_sequential();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	try
+	{
+		test_big_addRange();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+
 	return 0;
 }
